e4bd8919320433cc26642dd3fe4f8c34774dd7b3
yvanni <yva.munoz@duocuc.cl>
2026-03-31 10:26:10 -0300

    first commit

---
+++ src/main/java/cl/duoc/YvanniMunoz/YvanniMunozApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.YvanniMunoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YvanniMunozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YvanniMunozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/YvanniMunoz/YvanniMunozApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.YvanniMunoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YvanniMunozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
